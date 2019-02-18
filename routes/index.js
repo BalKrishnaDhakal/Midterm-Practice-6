16e3c6bfdc14f5c6e06aa5aef4cdc73a3773a9b7
Bal krishna Dhakal <bdhakal1@my.centennialcollege.ca>
2019-02-18 15:39:20 -0500

    added express support

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Home' });
 });
-
+// GET about page. */
+router.get('/about', function(req, res, next) {
+    res.render('index', { title: 'About' });
+});
+/* GET products page. */
+router.get('/products', function(req, res, next) {
+    res.render('index', { title: 'Products' });
+});
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+    res.render('index', { title: 'Services' });
+});
+/* GET home page. */
+router.get('/contact', function(req, res, next) {
+    res.render('index', { title: 'Contact' });
+});
 module.exports = router;
