b1728b3c8e1f11e3ddb52aa2c2a434e004c1f5dd
Bal krishna Dhakal <bdhakal1@my.centennialcollege.ca>
2019-02-19 23:00:52 -0500

    created contact models

---
+++ routes/contact.js
@@ -0,0 +1,32 @@
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+
+// Create a reference to the database schema
+
+let contact = require('../models/contact');
+
+/**
+ * Get Contact List Page - READ Operation
+ */
+router.get('/', (req, res, next) => {
+    contact.find((err, contactList) => {
+        if (err) {
+            return console.error(err);
+        } else {
+            console.log(contactList);
+
+            /*
+            res.render('contacts/index',{
+                title: 'Contact List',
+                contactList: contactList
+            });
+            */
+        }
+
+    });
+});
+
+
+module.exports = router;
